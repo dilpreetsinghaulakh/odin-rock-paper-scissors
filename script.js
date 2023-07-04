23078098d60949184ac621f514442f9f3732c897
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2023-07-04 19:27:07 +0530

    Add functions to script.js. Test function is just for testing as assumable

---
+++ script.js
@@ -0,0 +1,60 @@
+const getComputerChoice = () => {
+    return  Math.floor(Math.random() * 3);
+}
+
+const getPlayerChoice = () => {
+    var playerChoice = prompt("Enter your Choice (i.e Rock, Paper or Scissors) :");
+
+    playerChoice = playerChoice.toLowerCase();
+
+    var numPlayerChoice = -1;
+
+    switch (playerChoice) {
+        case "rock" :
+            numPlayerChoice = 0;
+            break;
+        case "paper" :
+            numPlayerChoice = 1;
+            break;
+        case "scissors" :
+            numPlayerChoice = 2;
+            break;
+        default :
+            console.log("Enter correct option");
+            break;
+    }
+    return numPlayerChoice;
+}
+
+const playRound = () => {
+    var computerSelection = getComputerChoice();
+
+    const playerSelection = getPlayerChoice();
+
+    console.log(playerSelection)
+    console.log(computerSelection)
+
+    if (playerSelection === computerSelection) {
+        console.log("Draw");
+    }
+
+    else if(playerSelection === 0 && computerSelection === 2) {
+        console.log("You won")
+    }
+
+    else if(playerSelection === 1 && computerSelection === 0) {
+        console.log("You won")
+    }
+
+    else if(playerSelection === 2 && computerSelection === 1) {
+        console.log("You won")
+    }
+
+    else {
+        console.log("You lose")
+    }
+}
+
+const test = () => {
+    playRound();
+}
